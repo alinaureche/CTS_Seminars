44b04f444630648c28f301bb94819327ab1ea30a
alinaureche <95219186+alinaureche@users.noreply.github.com>
2022-03-31 18:00:44 +0300

    Add files via upload

---
+++ Seminar_5_CTS/src/builder/Main.java
@@ -0,0 +1,12 @@
+package builder;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		User u1 = new User("alina@csie.ro", "alina", "ureche");
+		
+	}
+
+}
---
+++ Seminar_5_CTS/src/s5/singleton/v2/Database.java
@@ -0,0 +1,19 @@
+package s5.singleton.v2;
+
+public class Database {
+	//VARIANTA 2 EARLY
+		//o singura instanta => STATIC
+		private static Database INSTANCE= new Database(); // early initialization, nu e gresit insa se consuma memorie chiar daca nu e fol instanta
+					// nu e ok pt examen
+		static {
+			INSTANCE =new Database();
+		}
+		
+		private Database() {
+			
+		}
+		public Database getInstance() {
+			return INSTANCE; //nu se fol this pt ca nu e la nivelul obiectului, ci la nivelul clasei
+		}
+
+}
